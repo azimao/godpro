1d4ce43a690b2f1a921e2f123f90a2606535703f
azimao <599287041@qq.com>
2017-05-11 02:03:55 +0800

    修改orm处理from的方式

--- azimao/sql.php
+++ azimao/sql.php
@@ -3,14 +3,13 @@
 class orm{
     public $sql=[
         'select'=>'select ',
-        'from'=>' from ',
+        'from'=>'from ',
     ];
     function select(){
                     //获取传进来的参数
         $fields = func_get_args();
         foreach ($fields as $field){
-            if (trim($this->sql[__FUNCTION__]) != 'select') $this->sql[__FUNCTION__] .= ',';
-            $this->sql[__FUNCTION__] .= $field;
+            $this->_add(__FUNCTION__,$field);
         }
         return $this;
     }
@@ -21,9 +20,25 @@ class orm{
     }
 
     function from($tableName){
-
-        $this->sql[__FUNCTION__] .= $tableName;
+        $this->_add(__FUNCTION__,$tableName);
         return $this;
+    }
+
+
+/*
+    key是操作函数
+    str是操作函数传的字段
+    spliter是分隔符
+*/
+    function _add($key,$str,$spliter=','){
+        if (!$this->sql[$key]) return;
+
+        if (is_array($this->sql[$key])) {
+
+        }
+
+        if (trim($this->sql[$key]) != 'select') $this->sql[$key] .= $spliter;
+        $this->sql[$key] .= $str;
     }
 
 //    魔力函数 对象变成字符串
